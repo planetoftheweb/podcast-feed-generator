5baf6a98db4968480c473aac4a3ef7b0ef250e96
Ray Villalobos <ray@planetoftheweb.com>
2023-04-23 23:36:30 -0400

    changed entrypoint to python3

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python /usr/bin/feed.py
+python3 /usr/bin/feed.py
 
 # Commit the changes
 git add -A && git commit -m "Updated Feed"
