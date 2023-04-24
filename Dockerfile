35baf9bb5dbdb4d1003a0f9c0b3ed059744da1f7
Ray Villalobos <ray@planetoftheweb.com>
2023-04-23 21:44:51 -0400

    Using Entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,9 @@
 FROM ubuntu:latest
-COPY feed.py /feed.py
-ENTRYPOINT ["/feed.py"]
+RUN apk --update add git
+RUN apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python
+RUN python3 -m ensurepip
+RUN pip3 install --no-cache --upgrade pip setuptools
+RUN pip3 install pyyaml --no-cache
+COPY feed.py /usr/bin/feed.py
+COPY entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "==================================="
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python /usr/bin/feed.py
+
+# Commit the changes
+git add -A && git commit -m "Updated Feed"
+
+# push the branch to the repository origin
+git push --set-upstream origin main
+
+echo "==================================="
